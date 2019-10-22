7a992aeb69d745357347e665da6f3a8a717b3b2e
Madsen Servius <smervius@gmail.com>
2019-10-22 11:15:59 -0500

    set to production

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    # config.settings.production to config.settings.local if running in local
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
     try:
         from django.core.management import execute_from_command_line
