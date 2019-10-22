27f850dcec7f31d3dfe9519d5304bdee3d090481
Madsen Servius <smervius@gmail.com>
2019-10-22 12:04:59 -0500

    remove sentry-debug

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404, handler500
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 urlpatterns = [
-    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('auth/', include('apps.accounts.urls')),
     #path('pages/', include('apps.pages.urls')),
