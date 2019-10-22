efc73a23cf8639d5798e31b6a82ce94674062023
Madsen Servius <smervius@gmail.com>
2019-10-22 12:06:05 -0500

    set to production

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     # config.settings.production to config.settings.local if running in local
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
