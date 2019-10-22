569d2c194137d6db527a2ea9b7c68cc9b63b29de
Madsen Servius <smervius@gmail.com>
2019-10-22 07:54:18 -0500

    logout url

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as authViews
 
 from . import views
 
 urlpatterns = [
     path('', views.login, name="login"),
     path('register', views.register, name="register"),
+    path('logout', authViews.LogoutView.as_view(), name="logout"),
 ]
